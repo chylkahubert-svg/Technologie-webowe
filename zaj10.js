fc33027367f82e8ee06f48b98edfd2b576287c97
Hubert Chylka <chylkahubert@gmail.com>
2026-05-07 11:32:59 +0200

    zaj10

---
+++ zaj10.js
@@ -0,0 +1,18 @@
+function btn_anim(){
+    // let anim = document.getElementById
+    // let anim = document.querySelector
+    let anim = document.getElementById("anim")
+    anim.style.animation = "none"
+}
+
+// function btn_anim2(){
+//     var anim2 = document.getElementById("anim2")
+//     anim2.style
+// }
+
+// function btn_anim(){
+//     // let anim = document.getElementById
+//     // let anim = document.querySelector
+//     let anim = document.getElementById("anim")
+//     anim.style.animation =
+// }
