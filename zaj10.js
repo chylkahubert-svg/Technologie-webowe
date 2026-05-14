b33c5cd824650713b8823bd8b6d405c1bc4de541
Hubert Chylka <chylkahubert@gmail.com>
2026-05-14 10:09:57 +0200

    zaj10_1

--- zaj10.js
+++ zaj10.js
@@ -4,6 +4,14 @@ function btn_anim(){
     let anim = document.getElementById("anim")
     anim.style.animation = "none"
 }
+var anim2 = document.getElementById("anim2")
+anim2.onmouseover = function () {
+    anim2.style.animationPlayState = "paused"
+}
+anim2.onmouseout = function () {
+    anim.style.animationPlayState = "running"
+}
+
 
 // function btn_anim2(){
 //     var anim2 = document.getElementById("anim2")
